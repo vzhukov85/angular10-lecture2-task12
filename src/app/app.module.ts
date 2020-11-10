e3ffb85b16c9bc7490824762f277d27f2986d1b9
erzhukov <erzhukov@i-teco.ru>
2020-11-11 00:29:13 +0300

    task 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlackComponent } from './black/black.component';
+import { WhiteComponent } from './white/white.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlackComponent,
+    WhiteComponent
   ],
   imports: [
     BrowserModule
